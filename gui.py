566c1f1a24377a879129d5acdbf241ae60a86512
xОскарКоды <34571767+xOscarCodes@users.noreply.github.com>
2022-10-16 19:04:49 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import time 
 from tkinter import *
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
@@ -7,6 +8,14 @@ GPIO.setup(12, GPIO.OUT)
 GPIO.setup(16, GPIO.OUT)
 
 def rgb(rgb):
+    """Convert RGB colour coding to hexadecimal string using this function. 
+
+    Args:
+        rgb (int): rgb colour tuple
+
+    Returns:
+        string: hexadecimal string representing a colour
+    """
     return "#%02x%02x%02x" % rgb
 
 root = Tk()
@@ -15,9 +24,13 @@ root.minsize(width=400, height=150)
 root.geometry("400x200")
 root.config(bg= rgb((47, 46, 47)))
 
+# A special variable in the tkinter module which we are using to store value of the 
+# button which is pressed 
 var = StringVar()
 
 def gui():
+    """This function will load the main GUI components 
+    """
     global redLEDbutton, greenLEDbutton, blueLEDbutton
     lable = Label(root, text= "PRESS BUTTON TO TURN ON LED", font=('Courier', 12, "bold"),fg= "white", bg= rgb((47, 46, 47)))
     lable.place(relx=0.11, rely=0.15, relwidth= 0.8, relheight= 0.1)
@@ -35,6 +48,12 @@ def gui():
     exitButton.place(relx= 0.398, rely= 0.67, relwidth= 0.20, relheight= 0.2)
 
 def turnOnLed(colour):
+    """This function takes the colour of the led as parameter and then turn ons the 
+    respective led
+
+    Args:
+        colour (string): Colour of the led to turn on
+    """
     if colour == "red":
         LEDred()
     elif colour == "blue":
@@ -43,6 +62,9 @@ def turnOnLed(colour):
         LEDgreen()
 
 def LEDred():
+    """Function to run on red led on and off
+    This function will turn on red led and will turn off all other leds
+    """
     if redLEDbutton["text"] == "RED OFF":
         GPIO.output(10, GPIO.LOW)
         GPIO.output(12, GPIO.LOW)
@@ -57,6 +79,9 @@ def LEDred():
         greenLEDbutton["text"] = "GREEN ON"
 
 def LEDblue():
+    """Function to run on blue led on and off
+    This function will turn on blue led and will turn off all other leds
+    """
     if blueLEDbutton["text"] == "BLUE OFF":
         GPIO.output(12, GPIO.LOW)
         GPIO.output(10, GPIO.LOW)
@@ -71,6 +96,9 @@ def LEDblue():
         greenLEDbutton["text"] = "GREEN ON"
 
 def LEDgreen():
+    """Function to run on green led on and off
+    This function will turn on green led and will turn off all other leds
+    """
     if greenLEDbutton["text"] == "GREEN OFF":
         GPIO.output(16, GPIO.LOW)
         GPIO.output(12, GPIO.LOW)
@@ -85,6 +113,11 @@ def LEDgreen():
         blueLEDbutton["text"] = "BLUE ON"
 
 def exit():
+    """This function wiil set pin 10, 12 and 16 to low and 
+    then it will set all the pins of raspberry pi to its default state 
+    which is input state. After that it will distroy the root instance 
+    and it's widgets. 
+    """
     GPIO.output(10, GPIO.LOW)
     GPIO.output(12, GPIO.LOW)
     GPIO.output(16, GPIO.LOW)
@@ -92,6 +125,5 @@ def exit():
     root.destroy()
 
 gui()
-
 
 root.mainloop()
